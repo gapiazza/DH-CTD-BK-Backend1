1262f78078d17b4505aff7171d8c5b30e5517181
GonzaLo <gonzalo.piazza@gmail.com>
2023-03-06 21:11:33 -0300

    Actualización 12

---
+++ Parcial1/Alt3/catalog-service/src/main/java/com/dh/catalog/client/MovieServiceClient.java
@@ -0,0 +1,33 @@
+package com.dh.catalog.client;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+// @FeignClient(name="movie-service",url = "http://localhost:8085/api-movie")
+@FeignClient(name="movie-service")
+@LoadBalancerClient(value = "movie-service")
+public interface MovieServiceClient {
+
+	@GetMapping("/api/v1/movies/{genre}")
+	List<MovieDto> getMovieByGenre(@PathVariable (value = "genre") String genre);
+
+
+	@Getter
+	@Setter
+	class MovieDto{
+		private Long id;
+
+		private String name;
+
+		private String genre;
+
+		private String urlStream;
+	}
+
+}
