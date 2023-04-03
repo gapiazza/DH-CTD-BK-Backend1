0fdb3e52b74dbb0abf682c349bcbc26eed2b5222
GonzaLo <gonzalo.piazza@gmail.com>
2023-04-03 00:49:09 -0300

    Actualiación

---
+++ Parcial1/Alt6 (final)/catalog-service/src/test/java/com/dh/catalog/CatalogServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.dh.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
